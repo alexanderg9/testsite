ba6ca80c283a03812b82deac7b727df970cf210e
Graham <alexanderg9@mymacewan.ca>
2018-07-10 12:01:39 -0600

    before deleting os

--- jenswebsite/server.go
+++ jenswebsite/server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	gomail "gopkg.in/gomail.v2"
 )
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,18 @@ func main() {
 	setRoutes(r)
 	err := http.ListenAndServe(":8008", r)
 	if err != nil {
+		panic(err)
+	}
+	m := gomail.NewMessage()
+	m.SetHeader("From", "alexanderg9@mymacewan.ca")
+	m.SetHeader("To", "alexanderg9@mymacewan.ca")
+	m.SetHeader("Subject", "Hello!")
+	m.SetBody("text/html", "Hello")
+
+	d := gomail.NewDialer("smtp.example.com", 587, "user", "123456")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
 }
